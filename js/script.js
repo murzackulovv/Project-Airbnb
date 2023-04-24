b975c50e7a9fdf9ebf537f34586cb28db147943d
murzackulov-beka <105661806+murzackulov-beka@users.noreply.github.com>
2023-04-24 19:03:55 +0600

    [PM-#0.4]:add-search-and-sort-component

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 import { Header } from './header.js';
+import { SearchAndSort } from './searchAndSort.js';
+
+
 const createApp = () => {
   const header = Header()
   document.body.append(header)
+
+  const searchSort = SearchAndSort()
+  document.body.append(header, searchSort)
 }
 createApp();
